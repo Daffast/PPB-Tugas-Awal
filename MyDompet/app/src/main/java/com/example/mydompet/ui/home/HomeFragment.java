bfe937dccf172435a0f7e3c2bda030f877aa24ae
Daffast <80727899+Daffast@users.noreply.github.com>
2022-05-10 18:23:53 +0700

    Update Notif (Belum Fix)

--- MyDompet/app/src/main/java/com/example/mydompet/ui/home/HomeFragment.java
+++ MyDompet/app/src/main/java/com/example/mydompet/ui/home/HomeFragment.java
@@ -1,6 +1,10 @@
 package com.example.mydompet.ui.home;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +19,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mydompet.R;
 import com.example.mydompet.Transaksi;
+import com.example.mydompet.ui.notification.NotificationJobService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
   private HomeViewModel homeViewModel;
+  private JobScheduler mScheduler;
+  private static int Total;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,8 +67,24 @@ public class HomeFragment extends Fragment {
 
     homeViewModel.getAllTransaksi().observe(getViewLifecycleOwner(), transaksis -> {
       adapter.submitList(transaksis);
+      createNotification(transaksis);
     });
 
     return root;
+  }
+
+  public void createNotification(List<Transaksi> data){
+    //TODO: add notification service to all data
+    for(Transaksi transaksi : data) {
+      if (!data.isEmpty()) {
+        Total = transaksi.getNominalTransaksi();
+        ComponentName serviceName = new ComponentName(getContext(), NotificationJobService.class.getName());
+        PersistableBundle bundle = new PersistableBundle();
+        bundle.putInt("name",Total);
+        JobInfo.Builder builder = new JobInfo.Builder(0, serviceName).setExtras(bundle);
+        JobInfo myJobInfo = builder.build();
+        mScheduler.schedule(myJobInfo);
+      }
+    }
   }
 }
